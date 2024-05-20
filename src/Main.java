57de88e8bea835d08fd23830e4271804aa1961df
CristianMartu <martuccicristian18@gmail.com>
2024-05-20 14:59:15 +0200

    main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Bla bla bla!");
+
+        long numeroInteroLunghissimo = 100000;
+
+        System.out.println(numeroInteroLunghissimo);
+    }
+}
