2802645b073d2b8348245cd1cc065754a4e205e3
CristianMartu <martuccicristian18@gmail.com>
2024-05-20 17:12:48 +0200

    change name

--- src/Esercizio3.java
+++ src/Esercizio3.java
@@ -7,8 +7,8 @@ public class Esercizio3 {
     public static double perimetroRettangolo(double l1, double l2){
         return (l1+l2)*2;
     }
-    public static int pariOdispari(int num1){
-        return num1 % 2 == 0 ? 0 : 1;
+    public static int pariOdispari(int num){
+        return num % 2 == 0 ? 0 : 1;
     }
     public static double areaTriangolo(double a, double b, double c){
         double p = (a + b + c)/2;
--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,26 @@ public class Main {
         System.out.println(moltiplica(3, 2));
         System.out.println(concatena("Concatena una stringa + intero: ", 2));
 
-        String[] cinqueElementi = {"Ciao", "come", ",tutto", "bene", "?"};
-        String[] result = inserisciInArray( cinqueElementi , "stai");
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
+        String[] fiveElement = {"Ciao", "come", ",tutto", "bene", "?"};
+        String[] result = inserisciInArray( fiveElement , "stai");
+        for (String s : result) {
+            System.out.println(s);
         }
     }
     public static int moltiplica(int a, int b){
         return a * b;
     }
-    public static String concatena(String stringa, int number){
-        return stringa + number;
+    public static String concatena(String string, int number){
+        return string + number;
     }
-    public static String[] inserisciInArray(String[] array, String stringa){
+    public static String[] inserisciInArray(String[] array, String string){
         String[] result = new String[6];
-
         for (int i = 0; i < result.length; i++) {
             if (i < 2){
                 result[i] = array[i];
             }
             else if (i == 2){
-                result[i] = stringa;
+                result[i] = string;
             }
             else {
                 result[i] = array[i - 1];
